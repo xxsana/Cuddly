56357f0360af7d1b8778bcb4f0897127e328ae23
Sana <sanaJeong.dev@gmail.com>
2021-06-16 22:45:42 +0900

    git mistake

--- Cuddly/Utils/ImageNamingHelper.swift
+++ Cuddly/Utils/ImageNamingHelper.swift
@@ -2,18 +2,7 @@
 //  ImageNamingHelper.swift
 //  Cuddly
 //
-//  Created by Haru on 2021/06/01.
+//  Created by Haru on 2021/06/13.
 //
 
-import UIKit
-
-struct ImageNamingHelper {
-    func creatDesc(with id: String, to: Int) -> [UIImage?]{
-        var images: [UIImage?] = []
-        
-        for i in 1...to {
-            images.append(UIImage(named: "\(id)_\(i)"))
-        }
-        return images
-    }
-}
+import Foundation
